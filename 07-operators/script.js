2d9fdbb166896f6ce4f53ee3dbe9d5cccbfd1882
Jem Dawson <thejemmeh@gmail.com>
2025-01-13 22:03:01 -0800

    07 operators 1:32:52

---
+++ 07-operators/script.js
@@ -0,0 +1,38 @@
+let x;
+
+x = 5 + 5;
+x = 5 - 5;
+x = 5 * 5;
+x = 5 / 5;
+x = 5 % 5;
+
+//concatenation
+
+x = 'Hello' + ' ' + 'World';
+
+// Exponent
+x = 2 ** 3;
+
+// Increment
+x = 1;
+x++;
+x = x + 1;
+
+// decrement
+x = x - 1;
+x--;
+
+// Assignment Operators
+x = 10;
+
+x += 5;
+x -= 5;
+x *= 5;
+
+// Comparison Operators
+
+x = 2 == 2;
+
+x = 2 != 2;
+
+console.log(x);
